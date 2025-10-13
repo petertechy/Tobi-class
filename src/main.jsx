c538dd295514f1fac2d59ceb3329e0186cd85b29
Petertechy <petertechy01@gmail.com>
2025-10-13 23:08:08 +0100

    Update main.jsx: configure app entry point with latest setup

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,21 @@ import './index.css'
 import App from './App.jsx'
 import "../node_modules/bootstrap/dist/css/bootstrap.css"
 import { BrowserRouter } from 'react-router-dom'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+import counterSlice  from './redux/counterSlice.js'
+
+
+export const store = configureStore({
+  reducer: {counterSlice}
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <BrowserRouter>
-     <App />
+     <Provider store={store}>
+      <App />
+     </Provider>
    </BrowserRouter>
   </StrictMode>,
 )
