5a5ff69703ccfa8572787316e9cb87298904b21f
Petertechy <petertechy01@gmail.com>
2025-10-14 22:16:02 +0100

    Update counterSlice.js: improve reducer logic and add reset feature

--- src/redux/counterSlice.js
+++ src/redux/counterSlice.js
@@ -1,25 +1,39 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+let savedState;
+try {
+  const data = localStorage.getItem("counterState");
+  savedState = data ? JSON.parse(data) : null;
+} catch (error) {
+  savedState = null;
+}
+
+const initialState = savedState || {
+  count: 0,
+  allStudents: [],
+  allProducts: []
+};
+
 export const counterSlice = createSlice({
-    name: 'counter',
-    initialState:{
-        count: 0,
-        allStudents: [],
-        allProducts: []
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.count += 1;
     },
-    reducers:{
-        increment: (state)=>{
-            state.count += 1
-        },
-        decrement: (state)=>{
-            state.count -= 1
-        },
-        increaseByNumber: (state, action)=>{
-            state.count += action.payload
-        }
-    }
-})
+    decrement: (state) => {
+      state.count -= 1;
+    },
+    increaseByNumber: (state, action) => {
+      state.count += action.payload;
+    },
+    saveStudent: (state, action) => {
+      state.allStudents.push(action.payload);
+    },
+  },
+});
 
-export const {increment, decrement, increaseByNumber} = counterSlice.actions
+export const { increment, decrement, increaseByNumber, saveStudent } =
+  counterSlice.actions;
 
-export default counterSlice.reducer
+export default counterSlice.reducer;
