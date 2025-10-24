1c67f371e926d1154b64d26957dd99aaf45b7882
Petertechy <petertechy01@gmail.com>
2025-10-24 19:42:27 +0100

    pwa added

--- src/pages/Ref.jsx
+++ src/pages/Ref.jsx
@@ -3,14 +3,20 @@ import React, { useRef } from 'react'
 useRef
 
 const Ref = () => {
-    const reference = useRef(null);
+    const reference = useRef();
 
-    console.log(reference.current);
-    reference.current = "Adeola"
+    // console.log(reference)
+
+  const changeName = () =>{
+    console.log(reference.current.innerText)
+    reference.current.innerText = "Hello Tobi"
+    reference.current.style.color = "blue"
+  }
 
   return (
     <div>
-        <h1>{reference.current}</h1>
+        <h1 ref={reference}>Hello World</h1>
+        <button onClick={changeName}>Change Name</button>
     </div>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,57 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+    includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Tobi Project',
+        short_name: 'TProject',
+        description: 'This is world class solution software',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ],
+        screenshots: [
+          {
+            src: "/screenshot-wide.png",
+            sizes: "1280x720",
+            type: "image/png",
+            form_factor: "wide"
+          },
+          {
+            src: "/screenshot-narrow.png",
+            sizes: "375x667",
+            type: "image/png",
+            form_factor: "narrow"
+          },
+        ]
+      }
+    })
+  ],
 })
+
+    
